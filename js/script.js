2adefe058333ec48e8967cf2817614fce735fcf6
GabrieleStentella96 <stentellagabriele222@gmail.com>
2022-05-10 20:41:00 +0200

    Week3 lez2

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ if(somma == 20) {
     alert('Bravo!')
 }else {
     document.getElementById('quiz1').innerHTML = 'sbagliata';
-    console.log('Risposta errata')
+    console.log('Risposta sbagliata')
     alert('Riprova, sarai più fortunato')
     
 
@@ -19,7 +19,7 @@ if (quoziente == 11) {
     alert('Ci sai fare con i numeri!')
 } else {
     document.getElementById('quiz2').innerHTML = 'sbagliata';
-    console.log('Risposta errata')
+    console.log('Risposta sbagliata')
     alert('Non ci siamo proprio...')
 
 
@@ -31,18 +31,18 @@ if (prodotto == 1089) {
     alert('Non stai deludendo le aspettative')
 } else {
     document.getElementById('quiz3').innerHTML = 'sbagliata';
-    console.log('Risposta errata')
+    console.log('Risposta sbagliata')
     alert('Sbagliato, ma non abbatterti!')
 
 }
 let sottrazione = prompt('200-99');
 if (sottrazione == 101) {
     document.getElementById('quiz4').innerHTML = 'giusta';
-    console.log('Risposta giusta')
+    console.log('Risposta sbagliata')
     alert('Complimenti!')
 } else {
     document.getElementById('quiz4').innerHTML = 'sbagliata';
-    console.log('Risposta errata')
+    console.log('Risposta sbagliata')
     alert('Fai bene i conti!')
 
 }
