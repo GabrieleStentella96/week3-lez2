3ec63a49a9a9f788fdcf6a54e47302ad8b83843f
GabrieleStentella96 <stentellagabriele222@gmail.com>
2022-05-11 08:45:15 +0200

    esercizio javascript, miniquiz

--- js/script.js
+++ js/script.js
@@ -9,10 +9,9 @@ if(somma == 20) {
     document.getElementById('quiz1').innerHTML = 'sbagliata';
     console.log('Risposta sbagliata')
     alert('Riprova, sarai più fortunato')
-    
+}
 
-
-}let quoziente = prompt('275/25');
+let quoziente = prompt('275/25');
 if (quoziente == 11) {
     document.getElementById('quiz2').innerHTML = 'giusta';
     console.log('Risposta giusta')
@@ -21,10 +20,9 @@ if (quoziente == 11) {
     document.getElementById('quiz2').innerHTML = 'sbagliata';
     console.log('Risposta sbagliata')
     alert('Non ci siamo proprio...')
+}
 
-
-
-}let prodotto = prompt('33*33');
+let prodotto = prompt('33*33');
 if (prodotto == 1089) {
     document.getElementById('quiz3').innerHTML = 'giusta';
     console.log('Risposta giusta')
@@ -35,10 +33,11 @@ if (prodotto == 1089) {
     alert('Sbagliato, ma non abbatterti!')
 
 }
+
 let sottrazione = prompt('200-99');
 if (sottrazione == 101) {
     document.getElementById('quiz4').innerHTML = 'giusta';
-    console.log('Risposta sbagliata')
+    console.log('Risposta giusta')
     alert('Complimenti!')
 } else {
     document.getElementById('quiz4').innerHTML = 'sbagliata';
@@ -47,4 +46,22 @@ if (sottrazione == 101) {
 
 }
 
- 
+const pgreco = 3.14;
+const e = 2.71828;
+let numero= 5;
+
+if(numero %2==0 || numero <= 4 || numero != 1) {
+ console.log(numero * pgreco)
+
+}else{
+   console.log(numero * e)
+}
+
+if(numero >=4 || numero <=5 || numero !=4 || ) {
+    console.log(numero * e)
+}else{
+    console.log(numer * pgreco)
+}
+
+
+
